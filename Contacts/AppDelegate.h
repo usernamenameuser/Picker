fbc8c89defe296e2fddb330ef637c13dd8eb0dfd
usernamenameuser <writeme@email.ua>
2017-03-20 12:50:16 +0200

    Initial Commit

---
+++ Contacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contacts
+//
+//  Created by user on 20.03.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts
+//
+//  Created by user on 20.03.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
